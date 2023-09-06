3303aea730236051ada2b5b408051d094a77df46
Allie Zhao <allie.k.zhao@gmail.com>
2023-09-06 13:22:56 -0700

    Refactor to use prop passing

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,9 +1,34 @@
 import React from "react";
 import Ticket from "./Ticket";
 
+const mainTicketList = [
+  {
+    names: 'Thato and Haley',
+    location: '3A',
+    issue: 'firebase no save record'
+  },
+  {
+    names: 'Donko and Bonko',
+    location: '3B',
+    issue: 'horse crimes'
+  },
+  {
+    names: 'Snuggly and aster',
+    location: 'C54',
+    issue: 'sleeby'
+  }
+];
+
 function TicketList() {
   return (
-    <Ticket />
+    <React.Fragment>
+      {mainTicketList.map((ticket, index) =>
+        <Ticket names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          key={index}/>
+      )}
+    </React.Fragment>
   );
 }
 
