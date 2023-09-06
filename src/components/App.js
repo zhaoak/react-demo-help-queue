190fda681cfcc9b1c1cdd2fb857f3074fe7f0a0c
Allie Zhao <allie.k.zhao@gmail.com>
2023-09-05 17:07:07 -0700

    Created static functional components

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
 import React from "react";
+import Header from "./Header";
+import TicketList from "./TicketList";
 
 function App() {
   const name = "Thato";
   const name2 = "Haley";
   return (
     <React.Fragment>
-      <h1>Help Queue</h1>
-      <h3>3a</h3>
-      <h3>{name} and {name2}</h3>
-      <p><em>Firebase entries not saving!</em></p>
-      <hr/>
+      <Header />
+      <TicketList />
     </React.Fragment>
   );
 }
