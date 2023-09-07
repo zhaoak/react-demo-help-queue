aa5112a50424d9f2af2a2718be370d47366e61c3
Allie Zhao <allie.k.zhao@gmail.com>
2023-09-06 18:05:07 -0700

    Missed adding components lol

---
+++ src/components/NewTicketForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewTicketForm(props) {
+  return (
+    <React.Fragment>
+      <h3>form go here</h3>
+    </React.Fragment>
+  );
+}
+
+export default NewTicketForm;
