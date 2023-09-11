11cfac92e7fdeae1d82b3134598291e99da50caa
Allie Zhao <allie.k.zhao@gmail.com>
2023-09-11 15:58:29 -0700

    Add delete ticket reducer func/test

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -10,27 +10,60 @@ describe('ticketListReducer', () => {
     id: 1
   };
 
+  const currentState = {
+    1: {
+      names: 'Rhone & Remy',
+      location: '4b',
+      issue: 'hypothetical issue',
+      id: 1
+    }, 2: {
+      names: 'Chrome & Firefox',
+      location: '4c',
+      issue: 'hypothetical issue',
+      id: 2
+    }
+  }
+  
+  // testing if returns current state
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(ticketListReducer({}, {type: null})).toEqual({});
   });
 
-    test('Should successfully add new ticket data to mainTicketList', () => {
-      const { names, location, issue, id } = ticketData;
-      action = {
-        type: 'ADD_TICKET',
+  // testing adding new ticket to state
+  test('Should successfully add new ticket data to mainTicketList', () => {
+    const { names, location, issue, id } = ticketData;
+    action = {
+      type: 'ADD_TICKET',
+      names: names,
+      location: location,
+      issue: issue,
+      id: id
+    };
+    
+    expect(ticketListReducer({}, action)).toEqual({
+      [id] : {
         names: names,
         location: location,
         issue: issue,
         id: id
-      };
-      
-      expect(ticketListReducer({}, action)).toEqual({
-        [id] : {
-          names: names,
-          location: location,
-          issue: issue,
-          id: id
-        }
-      });
+      }
     });
+  });
+
+  // testing deleting ticket from state
+  test('Should successfully delete a ticket', () => {
+    action = {
+      type: 'DELETE_TICKET',
+      id: 1
+    };
+    expect(ticketListReducer(currentState, action)).toEqual({
+      2: {
+        names: 'Chrome & Firefox',
+        location: '4c',
+        issue: 'hypothetical issue',
+        id: 2
+      }
+    });
+  });
+
 });
