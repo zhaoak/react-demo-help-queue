50346536797f3aa72e2479dee4600c64e6a01571
Allie Zhao <allie.k.zhao@gmail.com>
2023-09-25 15:50:39 -0700

    Implement toggling for theme state variable

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,16 @@ import { ThemeContext, themes } from '../context/theme-context';
 function App() {
   const [theme, setTheme] = useState(themes.light);
 
+  function toggleTheme() {
+    setTheme(theme =>
+      theme.textColor === "AntiqueWhite" ? themes.light : themes.dark
+    );
+  }
+
   return (
     <ThemeContext.Provider value={theme}>
       <Header />
-      <ToggleTheme />
+      <ToggleTheme toggleTheme={toggleTheme} />
       <TicketControl/>
     </ThemeContext.Provider>
   );
--- src/components/ToggleTheme.js
+++ src/components/ToggleTheme.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function ToggleTheme() {
+function ToggleTheme(props) {
   return (
     <React.Fragment>
-      <button type='button'>Toggle Theme</button>
+      <button type='button' onClick={props.toggleTheme}>Toggle Theme</button>
       <hr/>
     </React.Fragment>
   );
+}
+
+ToggleTheme.propTypes = {
+  toggleTheme: PropTypes.func
 }
 
 export default ToggleTheme;
