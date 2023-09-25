14a59fc673fd329f67e61a0605aec3de9a6a6246
Allie Zhao <allie.k.zhao@gmail.com>
2023-09-25 15:12:04 -0700

    Add ToggleTheme component and add to tree

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import TicketControl from "./TicketControl";
+import ToggleTheme from "./ToggleTheme";
 
 function App() {
   const name = "Thato";
@@ -8,6 +9,7 @@ function App() {
   return (
     <React.Fragment>
       <Header />
+      <ToggleTheme />
       <TicketControl/>
     </React.Fragment>
   );
---
+++ src/components/ToggleTheme.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ToggleTheme() {
+  return (
+    <React.Fragment>
+      <button type='button'>Toggle Theme</button>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default ToggleTheme;
