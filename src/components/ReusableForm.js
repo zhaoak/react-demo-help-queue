67c5e586f228f1c219d861094770a4a2b27b60f6
Allie Zhao <allie.k.zhao@gmail.com>
2023-09-25 16:33:21 -0700

    Add theme toggling styling to reusable form component

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,23 +1,39 @@
-import React from "react";
+import React, {useContext} from "react";
+import {ThemeContext} from '../context/theme-context';
 import PropTypes from "prop-types";
 
 function ReusableForm(props) {
+
+  const theme = useContext(ThemeContext);
+
+  const buttonStyles = {
+    backgroundColor: theme.buttonBackground,
+    color: theme.textColor
+  }
+
+  const inputStyles = {
+    backgroundColor: theme.inputBackground,
+    color: theme.textColor
+  }
 
   return (
     <React.Fragment>
      <form onSubmit={props.formSubmissionHandler}>
         <input
+          style={inputStyles}
           type='text'
           name='names'
           placeholder='Pair Names' />
         <input
+          style={inputStyles}
           type='text'
           name='location'
           placeholder='Location' />
         <textarea
+          style={inputStyles}
           name='issue'
           placeholder='Describe your issue.' />
-        <button type='submit'>{props.buttonText}</button>
+        <button style={buttonStyles} type='submit'>{props.buttonText}</button>
       </form>
     </React.Fragment>
   );
